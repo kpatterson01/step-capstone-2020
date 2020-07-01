98f5ace935f257523ce42042ffc49fa34da724c6
Dean Alvarez <dean.alvarez.24@gmail.com>
2020-07-01 20:31:15 +0000

    adds file cleanup

--- csv_sort.py
+++ csv_sort.py
@@ -1,16 +1,23 @@
-#This is an external sort for CSV files
-#Algorithm info:
+# This is an external sort for CSV files. It is suited for our specific dataset but could be modified
+# Algorithm info:
 # read and sort a chunk of the data using conventional methods
 # write sorted data to disk
 # repeat until all data is in sorted chunks
 # read small amount from each chunk
 # merge these mini-chunks
-#visit https://en.wikipedia.org/wiki/External_sorting for more info
+# In particular, I merge these mini-chunks with a min-heap. I load the first element from each chunk into the heap
+# then I pop the top of the minheap and add another item from the chunk that the item which was popped origninated from
+# complexity: nlog(n), the sorting part is obviously nlog(n) and the merging part is more or less a heap-sort so it
+# is also nlog(n).
+#
+#
+# visit https://en.wikipedia.org/wiki/External_sorting for more info
 
 import csv
 import os
 import pandas as pd
 import heapq
+import glob
 
 class Row:
     #this is a custom class for the row data in our user_resources dataset.
@@ -48,8 +55,12 @@ chunk_size = 100000
 input_path = os.path.expanduser("~/data/user_resources.csv")
 output_path = os.path.expanduser("./sorted_user_resources.csv")
 
+
 chunk_number = 0
 reader = pd.read_csv(input_path,chunksize=chunk_size)
+
+# chunk the csv into chunks of size chunk_size and sort these chunks by user_id
+# write these sorted chunks to temporary files
 for chunk in reader:
     sorted_chunk = chunk.sort_values(by=['user_id'])
     sorted_chunk.to_csv('tmp%d.csv'%chunk_number)
@@ -58,7 +69,6 @@ for chunk in reader:
 
 readers = []
 heap = []
-heapq.heapify(heap)
 for i in range(chunk_number):
     f = open('tmp%d.csv'%i)
     reader = csv.reader(f,delimiter=',')
@@ -68,8 +78,11 @@ for i in range(chunk_number):
     row_obj = Row(row[1],row[2],row[3],row[4],row[5],row[6],i)
     heapq.heappush(heap,row_obj)
 
-heapq.heapify(heap)
 
+# while the heap isn't empty, write the smallest row (sorted by user_if)
+# to the out file. Call this row r. 
+# if the chunk is not empty, add a row from the chunk r originated from
+# to the minheap 
 while(len(heap) > 0):
     min_item = heapq.heappop(heap)
     write_csv_out(min_item,output_path)
@@ -81,3 +94,7 @@ while(len(heap) > 0):
         heapq.heappush(heap,row_obj)
     except:
         pass
+
+#clean tmp files
+for f in glob.glob("./tmp*.csv"):
+    os.remove(f)
