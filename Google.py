08c0e0e4d42a34ff0918c4873630dad07d1d1d74
tedim52 <tedi.m52@gmail.com>
2020-07-15 11:22:09 -0400

    Minor changes to Google and Googler skeleton.

---
+++ Google.py
@@ -0,0 +1,86 @@
+
+# Defines a Google class that encapsulates managerial hierarchy tree
+# and provides several functions to manipulate and analyze the organizational hierarchy.
+
+class Google:
+    """ This class simulates the managerial hierarchy tree of Googlers.
+
+    The class takes in a list of Googlers and creates a tree structure that can be used
+    to calculate the 1) Distance between two Googlers based on the go/who properties and
+    2) Usage similarity based on resources they've accessed.
+
+    Attributes:
+        ceo: A Googler object with no manager_id acting as the head node of the tree.
+        num_googlers: An integer count of Googlers within the company.
+        googlers: An array of Googler objects with all Googlers in the company.
+    """
+
+    def __init__(self, googlers):
+        """ Creates Google company
+
+        Calls __create_company method which implements manager hierarchy tree based on the list of
+        Googlers and their attributes.
+
+        Args:
+            googlers: A list of Googler objects.
+        """
+        self.ceo = self.__create_company(googlers)
+        self.num_googlers = googlers.length()
+        self.googlers = googlers
+
+    def search(self, googler_id):
+        """ Searches for and returns the Googler object given their id.
+
+        Args:
+            googler_id: An integer representing the id of a googler.
+
+        Returns:
+            googler: The Googler object attached with the googler_id.
+        """
+        #TODO: Implement Googler Search function
+        return
+
+    def distance(self, googler_one, googler_two):
+        """ Returns the distance of two Googlers in the managerial hierarchy.
+
+        A lower distance corresponds to Googlers who have more similar attribtues.
+        A higher distance corresponds to Googlers who have less similar attributes.
+
+        Args:
+            googler_one: A Googler object.
+            googler_two: A Googler object.
+
+        Returns:
+            distance: An integer representing their similarity or distance in the tree.
+        """
+        #TODO: Implement function to determine distance between to Googlers.
+        return
+
+    def usage_similarity(self, googler_one, googler_two):
+        """ Returns a usage similarity metric of two Googlers in the managerial hierarchy based
+            off of the resources that Googler has accessed in a set period of time.
+
+        A lower number corresponds to Googlers who are closer together in the tree and have more similar attribtues.
+        A higher number corresponds to Googlers who are farther away in the tree and are less similar.
+
+        Args:
+            googler_one: A Googler object.
+            googler_two: A Googler object.
+
+        Returns:
+            metric: An integer representing their usage similarity.
+        """
+        #TODO: Implement function to determine distance between googler.
+        return smth
+
+    def __create_company(self, googlers):
+        """ Creates managerial hierarchy tree of Google company.
+
+        Args:
+            googlers: An list of Googler objects.
+
+        """
+        #TODO: Implement function to create managerial hierarchy.
+        return
+
+    #Add any required methods for the class
---
+++ Googler.py
@@ -0,0 +1,74 @@
+
+# Defines a Googler class encapsulating their information on go/who.
+
+class Googler:
+    """ This class encapsulates a Googlers information.
+
+    The class takes in attributes that represent information specific to every Googler working at
+    Google that can be found on their go/who. These attributes are anonymized integers
+    that represent each piece of information.
+
+    Attributes:
+        id
+        department
+        cost_center
+        manager_id
+        location
+        lowest_dir_id
+        job_family
+        resources: A list of integers that represent a specific resource.
+        reports: A list of Googler objects representing Googlers who report to this Googler object.
+    """
+
+    def __init__(self, attributes):
+        """Initializes Googler object based on anonymized attributes from go/who.
+
+        Args:
+            attributes: An JSON object of mapping attributes to integers that represent the attributes.
+                        Ex. attributes: { id: 12, job_family: 10 ...}
+                            id: 12 --> 854542/Intern
+                            job_family: 10 --> Intern-Technical
+                            ...
+        """
+        self.id = attributes.id
+        self.department = attributes.department
+        self.cost_center = attributes.cost_center
+        self.manager_id = attributes.manager_id
+        self.location = attributes.location
+        self.lowest_dir_id = attributes.lowest_dir_id
+        self.job_family = attributes.job_family
+        self.resources = []
+        self.reports = []
+
+    def add_report(self, googler):
+        """ Adds the googler to list of reports.
+
+        Args:
+            googler: A Googler object.
+        """
+        self.reports.append(googler)
+
+    def add_resource(self, resource):
+        """ Adds the resource to list of resources.
+
+        Args:
+            resources: An integer id representing the resource. **NOTE: Resource object? @Dean?**
+        """
+        self.resources.append(resource)
+
+    def get_attributes(self):
+        """ Returns attributes in a list.
+
+        Returns:
+            Attribute list. Ex. [1, 12, 1, 18, 1219, 220, 1234]
+        """
+        attr = []
+        attr.append(self.department)
+        attr.append(self.cost_center)
+        attr.append(self.manager_id)
+        attr.append(self.location)
+        attr.append(self.lowest_dir_id)
+        attr.append(self.job_family)
+        return attr
+
+    #Add any required methods for the class
