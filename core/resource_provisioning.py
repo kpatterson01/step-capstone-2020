a172b7d7c3496ff6e9c7250ea79c06189e4d6750
Dean Alvarez <dean.alvarez.24@gmail.com>
2020-07-24 17:26:31 +0000

    add rule with default values

--- core/resource_provisioning.py
+++ core/resource_provisioning.py
@@ -1,8 +1,9 @@
 # This file contains some useful functions for metrics regarding provisioning
 import pickle
 from employee import Employee
-from utilities.resource_map_creator import Resource
+from resource import Resource
 import csv
+from rule import Rule
 
 # gets provisioning metrics for a given resource given a rule
 # rule should be a function that takes a user and returns a boolean (examples below)
@@ -10,7 +11,7 @@ def get_metrics(resource, rule, resource_map, company):
     actual_users = resource_map[resource]
     predicted_users = set()
     for user in company:
-        if rule(user):
+        if rule.passes(user):
             predicted_users.add(user.id)
     print("actual: %d , predicted: %d"%(len(actual_users),len(predicted_users)))
     metrics = calculate_metrics(actual_users, predicted_users)
@@ -32,12 +33,12 @@ def calculate_metrics(actual, predicted):
     return precision, recall
 
 def load_resource_map():
-    LOAD_PATH = "../data/resource_map.pkl"
+    LOAD_PATH = "../../data/resource_employee_map.pkl"
     resource_map = pickle.load(open(LOAD_PATH,"rb"))
     return resource_map
 
 def load_company():
-    LOAD_PATH = "../data/users.csv"
+    LOAD_PATH = "../../data/users.csv"
     company = set()
     with open(LOAD_PATH, 'r') as f:
         reader = csv.reader(f)
@@ -45,31 +46,31 @@ def load_company():
         for user in reader:
             attributes = {}
             if user[0] == 'null':
-                attributes["id"] = None
+                attributes["id"] = -1
             else:
                 attributes["id"] = int(user[0])
             if user[1] == 'null':
-                attributes['department'] = None
+                attributes['department'] = -1
             else:
                 attributes["department"] = int(user[1])
             if user[2] == 'null':
-                attributes["cost_center"] = None
+                attributes["cost_center"] = -1
             else:
                 attributes["cost_center"] = int(user[2])
             if user[3] == 'null':
-                attributes["manager_id"] = None
+                attributes["manager_id"] = -1
             else:
                 attributes["manager_id"] = int(user[3])
             if user[4] == 'null':
-                attributes["location"] = None
+                attributes["location"] = -1
             else:
                 attributes["location"] = int(user[4])
             if user[5] == 'null':
-                attributes["lowest_dir_id"]  = None
+                attributes["lowest_dir_id"]  = -1
             else:
                 attributes["lowest_dir_id"]  = int(user[5])
             if user[6] == 'null':
-                attributes["job_family"] = None
+                attributes["job_family"] = -1
             else:
                 attributes["job_family"] = int(user[6])
             employee = Employee(attributes)
@@ -80,19 +81,12 @@ def load_company():
     return company
 
 
-def example_rule(user):
-    return user.department == 13
-
-def another_example_rule(user):
-    return user.id == 9
-
 if __name__ == "__main__":
     resource_map = load_resource_map()
     company = load_company()
 
     example_resource = Resource(411231,2072)
+    example_rule = Rule(None)
     metrics = get_metrics(example_resource, example_rule, resource_map, company)
     print(metrics)
 
-    metrics = get_metrics(example_resource, another_example_rule, resource_map, company)
-    print(metrics)
