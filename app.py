5b6fc6edcb5f186ad5d7e11d43009718c027dd8e
tedim52 <tedi.m52@gmail.com>
2020-07-31 11:47:25 -0400

    Change usage similarity to proportion

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #===================================================
 # S. A. M: Smarter Access Management
-# An internal tool to help employees make better decisions for security and peace of mind.
+# An internal tool to help employees make better access decisions for security and peace of mind.
 # STEP Capstone2020: Kayla Patterson, Dean Alvarez, Tedi Mitiku
 #===================================================
 from flask import Flask, request, render_template
@@ -21,8 +21,8 @@ company = pickle.load(open('./data/smalldata/small_company_object.pkl', 'rb'))
 employee_resources = json.load(open("./data/employee_resource_map.json", 'r'))
 
 # Provisioning Metrics data
-employee_set = set(company.employees)
-resource_employee_map = rp.load_resource_map_from_csv("./data/smalldata/small_resource_table.csv")
+employee_set = rp.load_company("./data/users.csv")
+resource_employee_map = rp.load_resource_map_from_csv("./data/user_resources.csv")
 
 # Response protocols
 def success_response(data, code=200):
@@ -45,6 +45,10 @@ def hierachy_tree():
 def provision_metrics():
     return render_template("provisionMetrics.html")
 
+@app.route("/research.html")
+def research():
+    return render_template("research.html")
+
 @app.route("/api/distance", methods=["POST"])
 def calculate_distance():
     body = json.loads(request.data)
@@ -63,7 +67,6 @@ def calculate_usage():
     try:
         employee_one = company.search(int(body.get("employee_one_id")))
         employee_two = company.search(int(body.get("employee_two_id")))
-        distance = company.distance(employee_one, employee_two)
     except Exception as e:
         print(e)
         return failure_response("One or more employees not found in company.", 500)
@@ -82,7 +85,12 @@ def calculate_usage():
             resource_set_one.add((resource[0], resource[1]))
         for resource in employee_two_resources:
             resource_set_two.add((resource[0], resource[1]))
-        usage_similarity = len(resource_set_one.intersection(resource_set_two))
+        num_in_common = len(resource_set_one.intersection(resource_set_two))
+        num_total = len(resource_set_one) + len(resource_set_two) - num_in_common
+        if(num_total == 0 or num_in_common == 0):
+            usage_similarity = 0
+        else:
+            usage_similarity = int((num_in_common/num_total)*100)
     return success_response(usage_similarity)
 
 @app.route("/api/provisioning", methods=["POST"])
